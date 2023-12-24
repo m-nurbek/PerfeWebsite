abef843c530594db7280e132f23fbcc00e566380
ArdakHere <mrglok1@gmail.com>
2023-12-25 00:30:35 +0600

    Started working on the animation of "Our Services" and "Team Description". No significant changes in other components have been made

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -1,34 +1,89 @@
-import '/src/assets/css/OurServices.css'; // Import your CSS file
+import React, { useEffect, useState } from 'react';
+import { animated, config, useInView, useSpring } from 'react-spring';
 
+function AnimatedComponentLeft({ children }) {
+  const [ref, inView] = useInView();
+
+  const [resetAnimation, setResetAnimation] = useState(false);
+
+  const springProps = useSpring({
+    from: { opacity: 0, transform: 'translateX(-100%)' },
+    to: { opacity: inView ? 1 : 0, transform: inView ? 'translateX(0)' : 'translateX(-100%)' },
+    config: config.molasses,
+    onRest: () => {
+      if (!inView && resetAnimation) {
+        setResetAnimation(false);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (!inView) {
+      // Reset the animation when out of view
+      setResetAnimation(true);
+    }
+  }, [inView]);
+
+  return <animated.div ref={ref} style={springProps}>{children}</animated.div>;
+}
+
+function AnimatedComponentRight({ children }) {
+  const [ref, inView] = useInView();
+
+  const [resetAnimation, setResetAnimation] = useState(false);
+
+  const springProps = useSpring({
+    from: { opacity: 0, transform: 'translateX(100%)' },
+    to: { opacity: inView ? 1 : 0, transform: inView ? 'translateX(0)' : 'translateX(100%)' },
+    config: config.molasses,
+    onRest: () => {
+      if (!inView && resetAnimation) {
+        setResetAnimation(true);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (!inView) {
+      // Reset the animation when out of view
+      setResetAnimation(true);
+    }
+  }, [inView]);
+
+  return <animated.div ref={ref} style={springProps}>{children}</animated.div>;
+}
 
 function OurServices() {
-    return (
-        <>
-            <h1 style={{fontFamily: "Montserrat", fontSize: "60px"}}>Our Services </h1>
+  const imageScale = 1.2;
 
-            <div className="container">
-              <div className="column">
-                  <img src="/src/assets/img/Service2.png"/>
-                  <div className="column">
-                      <h4 style={{fontFamily: "Montserrat", fontSize: "20px", marginRight: "160px", marginLeft: "160px"}}>We create websites from the ground up.</h4>
-                  </div>
+  return (
+    <>
+      <h1 style={{ fontFamily: "Montserrat", fontSize: "60px", marginBottom: '100px' }}>Our Services </h1>
 
-                  <img style={{marginTop:"40px"}} src="/src/assets/img/Service2.png"/>
-                  <div className="column">
-                      <h4 style={{fontFamily: "Montserrat", fontSize: "20px", marginRight: "160px", marginLeft: "160px"}}>We deliver personalized website designs.</h4>
-                  </div>
-              </div>
-                    <div className="column">
-                        <img src="/src/assets/img/Service2.png" style={{marginTop: "200px", transform: 'rotate(-90deg)'}}/>
-                          <div className="column">
-                              <h4 style={{fontFamily: "Montserrat", fontSize: "20px", marginRight: "160px", marginLeft: "160px", marginTop: "100px"}}>We revise existing websites & recommend improvements.</h4>
-                          </div>
-                    </div>
+      <div className="container">
+        <AnimatedComponentLeft>
+          <div className="column" >
+            <img src="/src/assets/img/Service2.png" style={{ transform: `scale(${imageScale})` }} />
+            <div className="column">
+              <h4 style={{ fontFamily: "Montserrat", fontSize: "20px", marginRight: "160px", marginLeft: "160px" }}>We create websites from the ground up.</h4>
             </div>
-
-        </>
-    );
-
+            <img style={{ marginTop: "60px", transform: `scale(${imageScale})` }} src="/src/assets/img/Service2.png" />
+            <div className="column">
+              <h4 style={{ fontFamily: "Montserrat", fontSize: "20px", marginRight: "160px", marginLeft: "160px" }}>We deliver personalized website designs.</h4>
+            </div>
+          </div>
+        </AnimatedComponentLeft>
+        <AnimatedComponentRight>
+          <div className="column">
+            <img src="/src/assets/img/Service2.png" style={{ marginTop: "200px", transform: `scale(${imageScale}) rotate(-90deg)` }} />
+            <div className="column">
+              <h4 style={{ fontFamily: "Montserrat", fontSize: "20px", marginRight: "160px", marginLeft: "160px", marginTop: "140px" }}>We revise existing websites & recommend improvements.</h4>
+            </div>
+          </div>
+        </AnimatedComponentRight>
+      </div>
+    </>
+  );
 }
 
 export default OurServices;
--- src/components/TeamDescription.tsx
+++ src/components/TeamDescription.tsx
@@ -1,13 +1,13 @@
-
+import React, { useEffect } from 'react';
 import Reveal from 'react-reveal/Reveal';
-
+import { animated, useInView, useSpring } from 'react-spring';
 
 const personCardStyle = {
-    height: "800px",
+  height: '800px',
   padding: '15px',
   textAlign: 'center',
-    opacity: 0,
-    transition: 'opacity 0.8s ease-in-out',
+  opacity: 0,
+  transition: 'opacity 0.8s ease-in-out',
 };
 
 const imageStyle = {
@@ -23,81 +23,78 @@ const linkStyle = {
 };
 
 interface Props {
-    name: string;
-    role: string;
-    description: string;
-    src: string;
-    link: string
+  name: string;
+  role: string;
+  description: string;
+  src: string;
+  link: string;
 }
 
-
 const PersonCard = ({ name, role, description, src, link }: Props) => {
+  const [ref, inView] = useInView();
+  const springProps = useSpring({
+    from: { opacity: 0, transform: 'translateY(24px)' },
+    to: { opacity: inView ? 1 : 0, transform: inView ? 'translateY(0)' : 'translateY(24px)' },
+  });
 
   return (
-      <div>
-        <Reveal effect="fadeInUp">
-        <div style={personCardStyle}>
-          <img src={src} alt={name} id="top_scroll" style={imageStyle} />
-          <div>
-            <h3 style={{fontFamily: 'Montserrat-Bold'}}>{name}</h3>
-            <p style={{fontSize: '15px', fontFamily: "Montserrat-Bold", fontWeight: "lighter"}}>{role}</p>
-            <p style={{fontFamily: "Montserrat"}}>{description}</p>
-            <a href={link} target="_blank" rel="noopener noreferrer" style={linkStyle}>
-              Learn more
-            </a>
-          </div>
+    <Reveal effect="fadeInUp">
+      <animated.div  style={{ ...springProps, ...personCardStyle }}>
+        <img src={src} alt={name} id="top_scroll" style={imageStyle} />
+        <div>
+          <h3 style={{ fontFamily: 'Montserrat-Bold' }}>{name}</h3>
+          <p style={{ fontSize: '15px', fontFamily: 'Montserrat-Bold', fontWeight: 'lighter' }}>{role}</p>
+          <p style={{ fontFamily: 'Montserrat' }}>{description}</p>
+          <a href={link} target="_blank" rel="noopener noreferrer" style={linkStyle}>
+            Learn more
+          </a>
         </div>
-        </Reveal>
-      </div>
+      </animated.div>
+    </Reveal>
   );
 };
 
-function TeamDescription(){
+function TeamDescription() {
   const peopleRowStyle = {
     display: 'flex',
     justifyContent: 'space-around',
   };
 
-
   return (
-      <>
-          <h1 style={{fontFamily: "Montserrat", fontSize: "60px"}}>Our Team </h1>
-        <div style={peopleRowStyle} >
-
-
-          <PersonCard
-            name="Dariya"
-            role="Project Manager"
-            description="Collects Deans' lists like infinity stones"
-            src='/src/assets/img/Dariya.jpg'
-            link="https://example.com/person1"
-          />
-          <PersonCard
-            name="Aldamzhar"
-            role="Fullstack Developer"
-            description="J&J champ"
-            src='/src/assets/img/Alda.jpg'
-            link="https://example.com/person2"
-          />
-          <PersonCard
-            name="Nurbek"
-            role="Backend Developer"
-            description="Backend+ML+CN+Frontend developer"
-            src='/src/assets/img/Nurbek.jpg'
-            link="https://example.com/person3"
-          />
-          <PersonCard
-            name="Ardak"
-            role="Frontend Developer"
-            description="Runway model"
-            src='/src/assets/img/Ardak.jpg'
-            link="https://example.com/person4"
-          />
-        </div>
-          </>
+    <>
+      <h1 style={{ fontFamily: 'Montserrat', fontSize: '60px' }}>Our Team </h1>
+      <div style={peopleRowStyle}>
+        <PersonCard
+          name="Dariya"
+          role="Project Manager"
+          description="Collects Deans' lists like infinity stones"
+          src="/src/assets/img/Dariya.jpg"
+          link="https://example.com/person1"
+        />
+        <PersonCard
+          name="Aldamzhar"
+          role="Fullstack Developer"
+          description="J&J champ"
+          src="/src/assets/img/Alda.jpg"
+          link="https://example.com/person2"
+        />
+        <PersonCard
+          name="Nurbek"
+          role="Backend Developer"
+          description="Backend+ML+CN+Frontend developer"
+          src="/src/assets/img/Nurbek.jpg"
+          link="https://example.com/person3"
+        />
+        <PersonCard
+          name="Ardak"
+          role="Frontend Developer"
+          description="Runway model"
+          src="/src/assets/img/Ardak.jpg"
+          link="https://example.com/person4"
+        />
+      </div>
+    </>
   );
 }
 
-
 export default TeamDescription;
-
