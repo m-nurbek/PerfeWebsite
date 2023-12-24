1e94df11486427fa2c881530b7da989975b7b29d
Nurbek <nurbek.malikov@nu.edu.kz>
2023-12-25 04:53:04 +0600

    Added Levitating animation that will be used for icons around desktop

---
+++ src/components/animation_kit/LevitatingObj.tsx
@@ -0,0 +1,31 @@
+import {animated, useSpring} from "@react-spring/web";
+import icon1 from "../../assets/img/icon1.png";
+import {ReactNode} from "react";
+
+interface Props {
+    children: ReactNode;
+    tension?: number;
+    friction?: number;
+    transform?: string;
+}
+
+function LevitatingObj({children, tension, friction, transform} : Props) {
+    const styles = useSpring({
+        from: {transform: 'translate3d(0, 0px, 0)'},
+        to: [
+            {transform: 'translate3d(0, -10px, 0)'},
+            {transform: transform ? transform : 'translate3d(0, 0px, 0)'},
+        ],
+        config: {tension: tension ? tension : 1, friction: friction ? friction : 0}, // Adjust these values to make the animation slower
+        loop: true,
+    });
+
+    const child = <img src={icon1} alt={"icon"}/>
+    return (
+        <animated.div style={styles}>
+            {children ? children : child}
+        </animated.div>
+    );
+}
+
+export default LevitatingObj;
